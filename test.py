b4f7e310855102acfd9db2f785805fc964228362
Santosh <you@example.com>
2026-03-23 22:45:50 -0700

    added code

---
+++ test.py
@@ -0,0 +1,12 @@
+athletes = [
+    "Cristiano Ronaldo",
+    "Lionel Messi",
+    "Roger Federer",
+    "Rafael Nadal",
+    "Virat Kohli",
+    "Sachin Tendulkar"
+]
+
+print("Famous Athletes:")
+for i, athlete in enumerate(athletes, 1):
+    print(f"{i}. {athlete}")
